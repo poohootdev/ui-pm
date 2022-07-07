c11b207fcf884a49b834c630a7ceaed3ed5dcdd5
poohootdev <poohoot.dev@gmail.com>
2022-07-08 05:13:07 +0900

    공사중 페이지로 수정

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/Home.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <a href="https://github.com/poohootdev/ui-pm" target="_blank" rel="noopener noreferrer">
+        <span className={styles.logo}> Copyright © 2022 poohoot.</span>
+      </a>
+    </footer>
+  );
+}
